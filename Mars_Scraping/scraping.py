737c1ff0ac73c1cd1ea9d756e78ca6ea1b67f238
crkaide <crkaide@ilstu.edu>
2021-05-19 10:00:05 -0500

    udpate Mars_Scraping/scraping.py

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_data": scrape_hemi_data(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,167 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def scrape_hemi_data(browser):
+    # ### Hemispheres
+
+    # BOILERPLATE
+
+    # Import Splinter and BeautifulSoup
+    from splinter import Browser
+    from bs4 import BeautifulSoup as soup
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    # Add'l Dependencies
+    import pandas as pd
+
+    # Set up executable path and URL for scraping
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # html = browser.html
+
+    # chal_soup = soup(html, 'html.parser')
+    # chal_soup
+
+    # Follow the steps below to use one common approach:
+
+    # Find the HTML tag that holds all the links to the 
+    # full-resolution images, or find a common CSS 
+    # element for the full-resolution image.
+    # "a.product-item h3"
+    hemisphere_profile = browser.find_by_css("a.product-item h3")
+
+    # Using a for loop, iterate through the tags or CSS 
+    # element.
+    for each in range(len(hemisphere_profile)):
+
+    # Create an empty dictionary, hemispheres = {}, 
+    # inside the for loop.
+        hemispheres = {}
+
+    # Use the for loop to complete the following actions:
+        
+    # a) click on each hemisphere link, 
+
+        # hemisphere_profile_ext[x].click()
+        # CANNOT USE EXTERNAL VARIBLE HERE?
+
+        browser.find_by_css("a.product-item h3")[each].click()
+
+    # b) navigate to the full-resolution image page,
+        full_res_page = browser.links.find_by_text("Sample").first
+        
+    # c) retrieve the full-resolution image URL string and title 
+    # for the hemisphere image, and 
+        hemispheres['img_url'] = full_res_page['href']
+        
+        # add title pair
+
+    # DO NOT ASSIGN VARIBLE HERE
+    #     title_pair_var = browser.find_by_css("h2.title")
+    #     hemispheres['title'] = title_pair_var.text
+                
+        hemispheres['title'] = browser.find_by_css("h2.title").text
+        
+        # send to OUTSIDE dictionary
+        hemisphere_image_urls.append(hemispheres)
+        
+    # d) use browser.back() to navigate back to the beginning 
+    # to get the next hemisphere image.
+        
+        browser.back()
+        
+    ### END FINAL CODE
+
+
+
+    # BELOW: PRIOR ATTEMPTS TO SOLVE
+
+    # html = browser.html
+    # chal_soup = soup(html, 'html.parser')
+    # parent_elem = chal_soup.find('div', 'collapsible results')
+
+    # title = parent_elem.find_all('h3')
+    # title
+
+    # hemisphere_image_urls_df = pd.DataFrame(hemisphere_image_urls)
+    # hemisphere_image_urls_df
+    # hemisphere_image_urls_df = hemisphere_image_urls_df.append(title)
+    # hemisphere_image_urls_df
+
+    # html_url_1_base = parent_elem.find_all('a')[0].get('href')
+    # html_url_2_base = parent_elem.find_all('a')[2].get('href')
+    # html_url_3_base = parent_elem.find_all('a')[4].get('href')
+    # html_url_4_base = parent_elem.find_all('a')[6].get('href')
+
+    # html_url_1 = (f"{url}{html_url_1_base}")
+    # html_url_2 = (f"{url}{html_url_2_base}")
+    # html_url_3 = (f"{url}{html_url_3_base}")
+    # html_url_4 = (f"{url}{html_url_4_base}")
+
+    # html_list = [html_url_1, html_url_2, html_url_3, html_url_4]
+
+    # html_series = pd.Series(html_list)
+    # html_series_df = pd.DataFrame(html_series)
+    # html_series_df['title'] = hemisphere_image_urls_df
+    # html_series_df.columns = ['hemisphere_url', 'title']
+    # html_series_df
+
+    # parent_elem = chal_soup.find_all('a', class_='itemLink product-item')
+    # parent_elem
+
+    # for each in parent_elem:
+    #     print(each)
+
+    #     # Hemisphere link ext
+
+    # # WHEN you reach the URL, find tag
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    # hemisphere_image_urls
+
+    hemisphere_image_urls
+
+    ### CHECK
+
+    # # 4. Print the list that holds the dictionary of each image url and title.
+    # hemisphere_image_urls
+    # [{'img_url': 'https://marshemispheres.com/images/full.jpg',
+    #   'title': 'Cerberus Hemisphere Enhanced'},
+    #  {'img_url': 'https://marshemispheres.com/images/schiaparelli_enhanced-full.jpg',
+    #   'title': 'Schiaparelli Hemisphere Enhanced'},
+    #  {'img_url': 'https://marshemispheres.com/images/syrtis_major_enhanced-full.jpg',
+    #   'title': 'Syrtis Major Hemisphere Enhanced'},
+    #  {'img_url': 'https://marshemispheres.com/images/valles_marineris_enhanced-full.jpg',
+    #   'title': 'Valles Marineris Hemisphere Enhanced'}]
+
+    # Note on final output cell:
+    # Common URL in check cell does not match the URL given in the module challenge
+    # This solution outputs img_url using the given URL
+    # If the "check" URL ('https://marshemispheres.com/images/') is necessary,
+    # I suggest using a for loop and string replace on the dictionary
+
+    # End the automated browsing session
+    browser.quit()
+
+
+
+
+
+# At the end of the function, return the scraped data as a list of 
+# dictionaries with the URL string and title of each hemisphere image.
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
